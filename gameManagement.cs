19a64a952928aa1bfbf72b3d8bceed80e5ee181d
Chendog03 <49842647+Chendog03@users.noreply.github.com>
2021-04-17 22:08:32 +0100

    Add files via upload

---
+++ gameManagement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManagement : MonoBehaviour
+{
+    public static gameManagement Singleton = null; // Validation if singleton already exists, clear it ready for startGame().
+
+    [SerializeField] public bool userInput = false; 
+    [SerializeField] public bool track = false; 
+    [SerializeField] public bool firstPerson = false; 
+
+    public playerKartMovement kartScript; // Getting script references from other game objects
+    public cameraMovement camScript;
+
+    [SerializeField] GameObject kartPrefab; // Getting kart prefab
+
+    public int generationNo; // Creating generation count
+
+    public int kartCount = 10; // Setting number of karts per evolution
+    public List<neuralKartMovement> karts = new List<neuralKartMovement>(); // Preparing a new list for kart script references
+
+    public neuralNet bestNeuralNetwork = null; // The best NeuralNetwork currently available
+    int bestFitness = -1; // The Fitness of the best NeuralNetwork ever created
+
+    public float gameSpeed = 1;
+
+    // Run racing game.
+    public void startGame() {
+        // Creating a singleton reference for the gameManagement script so that the program knows this is the only one.
+        if (Singleton == null) {
+            Singleton = this;
+        }
+
+        // Toggles the user input.
+        if (userInput) {
+            kartScript.setEnable(true);
+            camScript.userInput = true; 
+            Debug.Log("Enabled player kart.");
+        } else {
+            kartScript.setEnable(false);
+            camScript.userInput = false; 
+            Debug.Log("Disabled player kart.");
+        }
+
+        // Toggles whether the camera moves or not, required for first person.
+        if (track) {
+            camScript.track = true;
+        } else {
+            camScript.track = false;
+        }
+
+        // Toggles the perspective of the camera
+        if (firstPerson) {
+            camScript.firstPerson = true; 
+        } else {
+            camScript.firstPerson = false;
+        }
+
+        camScript.setEnable(true); // Allows the camera to run its movement script.
+
+        // Resetting all variables about the evolution to be default.
+        generationNo = 0;
+        bestNeuralNetwork = null;
+        bestFitness = -1;
+        karts = new List<neuralKartMovement>();
+
+        newGeneration();
+    }
+
+    // Run neural network training algorithm.
+    public void startTraining() {
+        userInput = false;
+
+        if (Singleton == null) {
+            Singleton = this;
+        }
+
+        // Resetting all variables about the evolution to be default.
+        generationNo = 0;
+        bestNeuralNetwork = null;
+        bestFitness = -1;
+        karts = new List<neuralKartMovement>();
+
+        newGeneration();
+    }
+
+    void newGeneration() {
+        generationNo++;
+
+        for (int i = 0; i < kartCount; i++) {
+            GameObject newKart = Instantiate(kartPrefab, transform);
+            karts.Add(newKart.GetComponent<neuralKartMovement>());
+        }
+    }
+
+    public void removeKart(neuralKartMovement deadKart) {
+        if (deadKart.fitness > bestFitness) {
+            bestFitness = deadKart.fitness;
+            bestNeuralNetwork = deadKart.network;
+        }
+
+        karts.Remove(deadKart);
+        Destroy(deadKart.gameObject); // Destroy the dead car
+        
+        if (karts.Count <= 0) // If there are no cars left
+            newGeneration(); // Create a new generation
+    }
+
+    public void changeKartNumber(string input) {
+        bool changedKartNumber = int.TryParse(input, out kartCount);
+
+        if (!(changedKartNumber)) {
+            Debug.Log("Input value is not of integer type.");
+            kartCount = 50;
+        } 
+    }
+
+    public void changeGameSpeed(float input) {
+        gameSpeed = input;
+    }
+}
---
+++ playerKartMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerKartMovement : MonoBehaviour {
+    public string guID { get; private set; } // Allows the system to reach the guID from outside the class.
+
+    // Sets global variables for the kart, adjustable from the editor.
+    public float driveSpeed = 3;
+    public float rotSpeed = 3;
+
+    public Rigidbody rb; // Allows reference to the karts rigidbody script.
+
+    public float fitness = 0; // Debugging use.
+
+    // Sets global variables for the kart, adjustable from the editor and public to the classes that record lap times.
+    public float startLap = 0;
+    public float endLap = 0;
+    public int lapCount = 0;
+
+    public gameManagement gameManagement; // Allows reference to the gameManager script used in onCollisionEnter().
+    
+    // Sets conditions for the physics engine (e.g. gravity) and sets the uuid for the kart. This should be in awake since the object has to be instantiated first.
+    void Awake() {
+        rb.useGravity = true;
+
+        guID = System.Guid.NewGuid().ToString();
+    }
+
+    // FixedUpdate calls every game frame. 
+    // Purely runs the move function using keyboard input (-1, 0 or 1) as paramters.
+    private void FixedUpdate() {
+        // For player kart controls see below
+        Move(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal")*Input.GetAxisRaw("Vertical"));
+    }
+
+    // Sets the velocity of the kart to the front facing vector times the vertical keyboard input times the drive speed.
+    // Sets the angular velocity (speed of rotation) to normal vector of the car times the keyboard input times the turn speed.
+    public void Move (float v, float h) {
+        float gameSpeed = gameManagement.Singleton.gameSpeed;
+
+        rb.velocity = transform.right * v * driveSpeed * gameSpeed;
+        rb.angularVelocity = transform.up * h * rotSpeed * gameSpeed;
+    }
+
+    // Increases the fitness for the kart when called from the checkpoint collision event.
+    public void checkpointHit() {
+        fitness++;
+        // Debug.Log(fitness);
+    }
+
+    // Event function that runs when the kart collides with another collider.
+    // The function checks that the collision was with a wall and logs that in the console. Debugging only for now.
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Environment")) { // If hits a wall
+            // Debug.Log("WALL HIT");
+            Debug.Log(gameManagement.Singleton.karts);
+        }
+    }
+
+    public void setEnable(bool torf) {
+        this.gameObject.SetActive(torf);
+    }
+}
+
+
